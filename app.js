bbefa733f94b59022ef0adbeb95c395cd7c394af
Divine Obeten <deevynobeeten@gmail.com>
2021-11-14 09:16:01 +0100

    Updating cart features

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const closeButton = document.getElementsByClassName("close")[0];
 const plusBtn = document.getElementsByClassName("plus")[0]; 
 const minusBtn = document.getElementsByClassName("minus")[0];
 const itemCount = document.getElementsByClassName("count")[0];
+const cartBtn = document.getElementsByClassName("cart")[0];
+const addToCart = document.getElementsByClassName("add-btn")[0];
+const cartCard = document.getElementsByClassName("cart-card")[0];
+const deleteBtn = document.getElementsByClassName("delete")[0];
+
 let count = 0;
 
 toggleButton.addEventListener('click', () => {
@@ -16,8 +21,20 @@ closeButton.addEventListener('click', () => {
 
 plusBtn.addEventListener('click', () => {
    itemCount.textContent = count++;
-}); 
+});
 
 minusBtn.addEventListener('click', () => {
    itemCount.textContent = --count;
+});
+
+cartBtn.addEventListener('click', () => {
+   cartCard.classList.toggle('toggle');
+});
+
+addToCart.addEventListener('click', () => {
+   alert("fuck off Pussio!");
+});
+
+deleteBtn.addEventListener('click', () => {
+   alert("Are you sure?");
 });
