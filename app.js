4c03904229c5a62ae3cac5f9642ecc134f1de7ef
Divine Obeten <deevynobeeten@gmail.com>
2021-11-14 11:31:06 +0100

    Updating cart features

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const itemCount = document.getElementsByClassName("count")[0];
 const cartBtn = document.getElementsByClassName("cart")[0];
 const addToCart = document.getElementsByClassName("add-btn")[0];
 const cartCard = document.getElementsByClassName("cart-card")[0];
-const deleteBtn = document.getElementsByClassName("delete")[0];
+const cardProduct = document.getElementsByClassName("prod")[0];
+const checkoutBtn = document.getElementsByClassName("checkout")[0];
+const emptyCart = document.getElementsByClassName("empty")[0]; 
 
 let count = 0;
 
@@ -20,7 +22,7 @@ closeButton.addEventListener('click', () => {
  });
 
 plusBtn.addEventListener('click', () => {
-   itemCount.textContent = count++;
+   itemCount.textContent = ++count;
 });
 
 minusBtn.addEventListener('click', () => {
@@ -32,9 +34,25 @@ cartBtn.addEventListener('click', () => {
 });
 
 addToCart.addEventListener('click', () => {
-   alert("fuck off Pussio!");
+   if(count > 1) {
+      cardProduct.innerHTML = `
+      <img src="/images/image-product-1-thumbnail.jpg" alt="cart item thumbnail" class="cart-thumb">
+      <div class="added-product">
+        <p>Autumn Limited Edition Sneakers</p>
+      <div class="quantity">
+        <p>$125.00 x ${count} <span class="total">$${125*count}</span></p>            
+      </div>
+      </div>
+      <img src="/images/icon-delete.svg" alt="delete cart item" class="delete">`
+      emptyCart.classList.toggle('disappear');
+      checkoutBtn.classList.toggle('appear');
+   } else {
+      alert("Choose a number");
+   }
 });
 
-deleteBtn.addEventListener('click', () => {
-   alert("Are you sure?");
-});
+// const deleteBtn = document.getElementsByClassName("delete")[0];
+
+// deleteBtn.addEventListener('click', () => {
+//    alert("Are you sure?");
+// });
